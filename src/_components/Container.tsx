91a8d9496112525a574dc9399ddff76a02f3862d
a7medabdelfadil <a7medabdelfadil@github.com>
2024-11-14 14:18:38 +0200

    fix

--- src/_components/Container.tsx
+++ src/_components/Container.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 
 const Container = ({ children }: { children: ReactNode }) => {
-  // TODO: Add padding
+  // TODO: Add padding if sideBar closed
   return <div className={`ml-3 mr-3 mt-5 lg:ml-[270px]`}>{children}</div>;
 };
 
